26acac01ca9e8e965aab795dcaf267a0789cd564
Mateus Mantovani <mateus.mantovani@luizalabs.com>
2020-02-26 19:05:43 -0300

    Create the menu to select the language

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 class App extends React.Component {
     state = { language: 'english' }
+
+    onLanguageChange = language => {
+        this.setState({ language });
+    }
+
     render() {
         return (
             <div className="ui container">
                 <div>
                     Select a language:
-                    <i className="flag us" />
-                    <i className="flag br" />
+                    <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
+                    <i className="flag br" onClick={() => this.onLanguageChange('portuguese')}/>
                 </div>
+                {this.state.language}
             </div>
         );
     }
