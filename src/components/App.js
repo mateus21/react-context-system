f9be46811bbc12b6b0c2b39062e4a382fad9449f
Mateus Mantovani <mateus.mantovani@luizalabs.com>
2020-02-26 18:59:18 -0300

    Create the base component

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+    state = { language: 'english' }
+    render() {
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag us" />
+                    <i className="flag br" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
