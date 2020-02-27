1d6f1491e4dff0ae6f60c9a92e496e3132442cff
Mateus Mantovani <mateus21mantovani@hotmail.com>
2020-02-26 22:51:48 -0300

    Create a Context Provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
 
 ReactDOM.render(
     <App />, document.querySelector('#root')
